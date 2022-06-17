2e6b8d3fc285b3493aab0bc2ec496895d0af649b
Romario Merin <merin.romario65@gmail.com>
2022-06-17 18:02:42 +0530

    Fix dependency issue

--- frontend/src/components/CollectorIP/CollectorDetails.js
+++ frontend/src/components/CollectorIP/CollectorDetails.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const CollectorDetails = () => {
     useEffect(() => {
