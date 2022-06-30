afcd3299ed70cde65256153f12a9c0dad532c444
Romario Merin <merin.romario65@gmail.com>
2022-06-30 13:02:16 +0530

    Remove unwanted headers

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,17 +12,12 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="white" variant="white">
             <Container>
-            <Navbar.Brand><img src="small_logo.png"></img> </Navbar.Brand>
-            <Navbar.Brand>Simlog</Navbar.Brand>
+            <Navbar.Brand><img src={require('../small_logo.png')}></img> </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto"></Nav>
                 <Nav>
-                <Nav.Link href="#deets">About</Nav.Link>
                 <NavDropdown title={user ? user.firstName : 'Sign In'} id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
-                    <NavDropdown.Divider />
                     <NavDropdown.Item onClick={onLogout}>Sign Out</NavDropdown.Item>
                 </NavDropdown>
                 </Nav>
