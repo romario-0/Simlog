fa7c36059c4122830b2c5b5f02483c219303f410
sadakat-tech <sadakatullasayed99@gmail.com>
2022-07-26 18:09:21 +0530

    simulation css updated

--- frontend/src/components/Jobs/JobTile.js
+++ frontend/src/components/Jobs/JobTile.js
@@ -1,13 +1,13 @@
 const JobTile = ({job, removeJob}) => {
     return (
-        <div>
+        <div className="container col-sm-7 jobtile simjoblist">
             <div>{job.jobName}</div>
             <div>{job.date}</div>
             <div>{job.status}</div>
             <div>{job.duration}</div>
             <div>{job.volume}</div>
             <div>{job.progress}</div>
-            <div><button onClick={() => removeJob(job._id)}>x</button></div>
+            <div><button className="btn-outline-warning"onClick={() => removeJob(job._id)}> x </button></div>
         </div>
     )
 }
