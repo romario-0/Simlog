a02055fb20147a663f7f1224e900c0f4098fe242
sadakat-tech <sadakatullasayed99@gmail.com>
2022-09-19 11:08:19 +0530

    new pull

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const cors = require("cors");
+
+var whitelist = ['http://localhost:3000', 'http://ec2-35-86-242-218.us-west-2.compute.amazonaws.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/',cors(corsOptions), function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
