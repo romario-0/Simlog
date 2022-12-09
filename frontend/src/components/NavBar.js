591302fec9abe0d3dea1f808aed428ec76dd219f
sadakat-tech <sadakatullasayed99@gmail.com>
2022-12-09 11:20:27 +0530

    pulled from development

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -10,6 +10,7 @@ const NavBar = () => {
             <NavLink to={'/'} >Home</NavLink>
             <NavLink  to={'/logLibrary'} >Log</NavLink>
             <NavLink  to={'/logTypes/0'} >Log Type</NavLink>
+            <NavLink  to={'/jobs/0'} >Dataset</NavLink>
             <NavLink  to={'/jobs/0'} >Jobs</NavLink>
             <NavLink  to={'/sources/0'} >Sources</NavLink>
             <NavLink  to={'/collectors/0'} >Collectors</NavLink>
