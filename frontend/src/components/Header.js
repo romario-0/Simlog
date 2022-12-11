1da7dcb823ec8814ad7349210f5adeab2ee2f8be
Romario Merin <merin.romario65@gmail.com>
2022-12-11 23:35:35 +0530

    Add route for dataset

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
 
-    const {user, onLogout} = useContext(AuthenticationContext);
+    const { user, onLogout } = useContext(AuthenticationContext);
     const navigate = useNavigate();
 
     const navigateToScreen = (path) => {
@@ -23,27 +23,28 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect fixed="top" expand="lg" bg="white" variant="white" className="container-fluid">
             <Container>
-            <Navbar.Brand><img src={require('../small_logo.png')}></img> </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto fs-5">
-                    {/* <Nav.Link onClick={() => navigateToScreen('/')}>Home</Nav.Link> */}
-                    <Nav.Link onClick={() => navigateToScreen('/simulations/0')}>Simulation</Nav.Link>
-                    <Nav.Link onClick={() => navigateToScreen('/logLibrary')}>Log</Nav.Link>
-                    <Nav.Link onClick={() => navigateToScreen('/logTypes/0')}>Log Type</Nav.Link>
-                    {/* <Nav.Link onClick={() => navigateToScreen('/jobs/0')}>Jobs</Nav.Link> */}
-                    <Nav.Link onClick={() => navigateToScreen('/sources/0')}>Sources</Nav.Link>
-                    <Nav.Link onClick={() => navigateToScreen('/collectors/0')}>Collectors</Nav.Link>
-                    <NavDropdown title="Administration" id="basic-nav-dropdown">
-                        <NavDropdown.Item onClick={() => navigateToScreen('/users/0')}>Users</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Nav>
-                <NavDropdown title={user ? user.firstName : 'Sign In'} id="collasible-nav-dropdown">
-                    <NavDropdown.Item onClick={handleLogout}>Sign Out</NavDropdown.Item>
-                </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
+                <Navbar.Brand><img src={require('../small_logo.png')}></img> </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto fs-5">
+                        {/* <Nav.Link onClick={() => navigateToScreen('/')}>Home</Nav.Link> */}
+                        <Nav.Link onClick={() => navigateToScreen('/simulations/0')}>Simulation</Nav.Link>
+                        <Nav.Link onClick={() => navigateToScreen('/logLibrary')}>Log</Nav.Link>
+                        <Nav.Link onClick={() => navigateToScreen('/logTypes/0')}>Log Type</Nav.Link>
+                        {/* <Nav.Link onClick={() => navigateToScreen('/jobs/0')}>Jobs</Nav.Link> */}
+                        <Nav.Link onClick={() => navigateToScreen('/sources/0')}>Sources</Nav.Link>
+                        <Nav.Link onClick={() => navigateToScreen('/collectors/0')}>Collectors</Nav.Link>
+                        <Nav.Link onClick={() => navigateToScreen('/dataset/0')}>Dataset</Nav.Link>
+                        <NavDropdown title="Administration" id="basic-nav-dropdown">
+                            <NavDropdown.Item onClick={() => navigateToScreen('/users/0')}>Users</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title={user ? user.firstName : 'Sign In'} id="collasible-nav-dropdown">
+                            <NavDropdown.Item onClick={handleLogout}>Sign Out</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
